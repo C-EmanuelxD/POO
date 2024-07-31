0bd72186e541efa36d757e9728979590d916bef8
Kayky <kaykykazuya@gmail.com>
2024-07-31 09:39:01 -0300

    Update GerenciadorMensagem.java

--- Trabalho/sistema-clinica2.0/src/Gerenciador/GerenciadorMensagem.java
+++ Trabalho/sistema-clinica2.0/src/Gerenciador/GerenciadorMensagem.java
@@ -1,16 +1,20 @@
 package Gerenciador;
 
 import Principais.Consulta;
+import Principais.Paciente;
 import Principais.Secretaria;
 import java.util.ArrayList;
 import java.util.List;
 
-//não feito ainda
 public class GerenciadorMensagem {
     private Secretaria secretaria;
+    private Email email;
+    private SMS sms;
     
     public GerenciadorMensagem(Secretaria secretaria) {
         this.secretaria = secretaria;
+        this.email = new Email();
+        this.sms = new SMS();
     }
 
     public Secretaria getSecretaria() {
@@ -23,11 +27,43 @@ public class GerenciadorMensagem {
     
     public void enviarEmail(String dataAtual) {
         List<Consulta> consultasMensagem = secretaria.consultasDiaSeguinte(dataAtual);
+        List<Consulta> consultasParaRemover = new ArrayList<>();
+        
+        int index = 0;
+        while (index < consultasMensagem.size()) {
+            Consulta consulta = consultasMensagem.get(index);
+            Paciente paciente = consulta.getPaciente();
+            if (paciente.getEmail() != null) {
+                email.enviaEmail(consulta);
+                consultasParaRemover.add(consulta);
+            } else {
+                index++;
+            }
+        }
+        
+        for (Consulta consulta : consultasParaRemover) {
+            secretaria.removeConsulta(consulta.getMedico().getCrm(), consulta.getData(), consulta.getHorario());
+        }
     }
     
     public void enviarSMS(String dataAtual) {
         List<Consulta> consultasMensagem = secretaria.consultasDiaSeguinte(dataAtual);
+        List<Consulta> consultasParaRemover = new ArrayList<>();
+        
+        int index = 0;
+        while (index < consultasMensagem.size()) {
+            Consulta consulta = consultasMensagem.get(index);
+            Paciente paciente = consulta.getPaciente();
+            if (paciente.getSms() != null) {
+                sms.enviaSMS(consulta);
+                consultasParaRemover.add(consulta);
+            } else {
+                index++;
+            }
+        }
+        
+        for (Consulta consulta : consultasParaRemover) {
+            secretaria.removeConsulta(consulta.getMedico().getCrm(), consulta.getData(), consulta.getHorario());
+        }
     }
-    
-    
 }
