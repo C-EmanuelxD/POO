0c2b5f1a4da685df608b840186a7087ea346c88a
Kayky <kaykykazuya@gmail.com>
2024-07-31 09:38:23 -0300

    Update Email.java

--- Trabalho/sistema-clinica2.0/src/Gerenciador/Email.java
+++ Trabalho/sistema-clinica2.0/src/Gerenciador/Email.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Gerenciador;
 
-/**
- *
- * @author Emanuel
- */
+import Principais.Consulta;
+
 public class Email {
-    
+        
+    public void enviaEmail(Consulta consulta) {
+        System.out.println("Olá, " + consulta.getPaciente().getNome() + "Deseja confirmar consulta para dia " + consulta.getData() + "no horário " + consulta.getHorario() + "com o médico " + consulta.getMedico().getNome() + "?");
+    }
 }
